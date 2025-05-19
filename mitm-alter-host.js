48c93e5466bbd777fc82a6df9a919c6f391fe1bf
datadiego <juandiegomariscal@gmail.com>
2025-05-19 18:53:34 +0200

    build

---
+++ mitm-alter-host.js
@@ -0,0 +1,30 @@
+import { Proxy } from "http-mitm-proxy";
+
+const proxy = new Proxy();
+
+proxy.onError((ctx, err) => {
+    console.error("proxy error:", err);
+});
+
+proxy.onRequest((ctx, callback) => {
+    const url = ctx.clientToProxyRequest.url;
+    const host = ctx.clientToProxyRequest.headers.host;
+    const method = ctx.clientToProxyRequest.method;
+
+    console.log("request:", { url, host, method });
+
+    // Si el host es Google, redirigir a Reddit
+    if (host === "www.google.com") {
+        console.log("🔄 Redirigiendo a Reddit");
+        ctx.proxyToClientResponse.writeHead(302, {
+            "Location": "https://www.reddit.com",
+        });
+        ctx.proxyToClientResponse.end();
+        return;
+    }
+
+    return callback();
+});
+
+console.log("Listening on 8081");
+proxy.listen({ port: 8081 });
